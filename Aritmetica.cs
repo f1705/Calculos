a665cd81fc47402678d268d65eb36ec98bb09821
Isabel Fernandes <45687384+f1705@users.noreply.github.com>
2024-01-20 22:32:20 +0000

    Adicionar comentários XML

--- Aritmetica.cs
+++ Aritmetica.cs
@@ -11,11 +11,20 @@ namespace Calculos
       
     internal class Aritmetica
     {
+        /// <summary>
+        /// Operação Soma.
+        /// </summary>
+        /// <returns>Retorna a soma de dois números.</returns>
+        
         public int Somar (int x, int y)
         { 
             return x + y; 
         }
 
+        /// <summary>
+        /// Operação Subtração.
+        /// </summary>
+        /// <returns>Retorna o resultado da subração de dois números.</returns>
         public int Subtrair(int x, int y)
         {
             return x - y;
